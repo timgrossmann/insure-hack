8628a115372ca91443ad05540bea737f7210195b
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-11-19 20:12:34 +0100

    Add policy lines

--- src/components/person-info.js
+++ src/components/person-info.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import CircularProgress from 'material-ui/CircularProgress';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
 import zurich from '../utils/zurich';
 
 
@@ -30,7 +32,7 @@ export default class PersonInfo extends Component {
 
   componentDidUpdate (prevProps) {
     if (prevProps.chat.insuranceID !== this.props.chat.insuranceID) {
-      loadCustomer();
+      this.loadCustomer();
     }
   }
 
@@ -41,7 +43,7 @@ export default class PersonInfo extends Component {
     let content;
 
     if (chat.insuranceID) {
-      content = <InsuranceDetails chat={chat} customer={customer} />
+      content = <InsuranceDetails chat={chat} customer={customer}/>
     } else {
       content = (
         <Card>
@@ -58,7 +60,7 @@ export default class PersonInfo extends Component {
       <div className='PersonInfo'>
 
         <div className='SubHeader'>
-          <h1>Info</h1>
+          <h1>Customer Details</h1>
           <FlatButton
             style={{ color: '#fff' }}
             label="close"
@@ -73,15 +75,17 @@ export default class PersonInfo extends Component {
   }
 }
 
-function InsuranceDetails ({chat, customer}) {
-  let details;
+function InsuranceDetails ({ chat, customer }) {
+  let details, policies;
   let name = chat.name;
 
+
   if (!customer) {
-    details = <div className='centered' ><CircularProgress /></div>;
+    details = <div className='centered'><CircularProgress /></div>;
   } else {
     let { FirstName, LastName, DateOfBirth } = customer.AccountHolderContact;
-    name = FirstName + ' ' + LastName;
+    //name = FirstName + ' ' + LastName;
+
     details = (
       <table className='InfoTable'>
         <tbody>
@@ -91,22 +95,109 @@ function InsuranceDetails ({chat, customer}) {
           </tr>
           <tr>
             <th>date of birth</th>
-            <td>{DateOfBirth}</td>
+            <td>{formatDate(DateOfBirth)}</td>
           </tr>
         </tbody>
       </table>
     );
+
+    policies = <PolicyDetails policies={customer.Policies}/>
   }
 
   return (
-    <Card>
-      <CardHeader
-        title={name}
-        avatar={chat.img}
-      />
-      <CardText>
-        {details}
-      </CardText>
-    </Card>
+    <div>
+      <Card>
+        <CardHeader
+          title={name}
+          avatar={chat.img}
+        />
+        <CardText>
+          {details}
+        </CardText>
+      </Card>
+
+
+      {policies}
+    </div>
+  )
+}
+
+function PolicyDetails ({ policies }) {
+  let policiesHTML = _.map(policies, (policy, i) => {
+
+    return (
+      <Card key={i}>
+        <CardHeader
+          title={`${formatProductCode(policy.ProductCode)} ${formatStatus(policy.LatestPeriod.Status)}`}
+          subtitle={`${formatDate(policy.LatestPeriod.PolicyStartDate)} - ${formatDate(policy.LatestPeriod.TermEndDate_ZDE)}`}
+          showExpandableButton={true}
+        />
+        <Divider/>
+        <CardText expandable={true}>
+          <PolicyLines latestPeriod={policy.LatestPeriod}/>
+        </CardText>
+      </Card>
+    );
+  });
+
+  return (
+    <div>
+      {policiesHTML}
+    </div>
+  );
+}
+
+function formatProductCode (code) {
+  return ({
+      'NPLPL_NewPersonalProduct': 'Private Insurance'
+    }[code]) || code;
+}
+
+function formatDate (string) {
+  if (!string) {
+    return '';
+  }
+
+  const [year, month, day] = string.split('-');
+  return `${day}.${month}.${year}`;
+}
+
+
+function formatStatus (status) {
+  return ({
+      tc_Draft: '(draft)',
+      tc_Expired: '(expired)',
+    }[status]) || '';
+}
+
+function formatPolicyLine (policyLine) {
+  return ({
+      UNLine: '',
+      GEBLine: '',
+      RSLine: '',
+      MSLine: '',
+      HALine: '',
+      HRLine: ''
+    }[policyLine]) || policyLine.substring(0, policyLine.length - 6);
+}
+
+function PolicyLines ({ latestPeriod }) {
+  const lineIds = _(latestPeriod)
+    .keys()
+    .filter((key) => _.endsWith(key, 'LineExists'))
+    .value();
+
+  const lines = _.pick(latestPeriod, lineIds);
+
+
+  const linesHTML = _.map(lines, (included, id) => {
+    return <li key={id}
+               className={included ? 'PolicyLineList__item--included' : 'PolicyLineList__item--excluded'}>{formatPolicyLine(id)}</li>;
+  });
+
+  return (
+    <ul className='PolicyLineList'>
+      {linesHTML}
+    </ul>
   )
 }
