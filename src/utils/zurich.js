31be26c9e285447535cd2623f536154dd3779296
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-11-20 09:37:51 +0100

    Remove fixed id

--- src/utils/zurich.js
+++ src/utils/zurich.js
@@ -3,12 +3,12 @@ import $ from 'jquery';
 const token = '131ac945-2259-3e6b-95c5-8035f3c30cb6';
 
 export default {
-  getCustomer () {
+  getCustomer (accountId) {
     return $.ajax({
       url: 'https://api.insurhack.com/apis/gi/1/Account_Set',
       data: {
         '$expand': 'AccountHolderContact,Policies($expand=LatestPeriod)',
-        '$filter': 'AccountNumber eq \'10000000024\''
+        '$filter': 'AccountNumber eq \'' + accountId + '\''
       },
 
       headers: {
